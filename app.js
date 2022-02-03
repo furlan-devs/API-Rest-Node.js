d235f2909a59b0d2c567964211beae959402c25e
Matheus Lemes <matheus311095@yahoo.com.br>
2022-02-02 22:12:25 -0300

    definindi o body e tratando o CORS

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-
+const bodyParser = require("body-parser");
 const rotaProdutos = require("./routes/produtos");
 const rotaPedidos = require("./routes/pedidos");
 
 app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false })); //apenas dados simples
+app.use(bodyParser.json()); //json de entrada no body
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Header", 
+  'Origin, X-Requerested-With, Content-Type, Accept, Authorization'
+  );
+
+  if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELET, GET');
+      return res.status(200).send({})
+  }
+  next();
+})
 
 app.use("/produtos", rotaProdutos);
 app.use("/pedidos", rotaPedidos);
@@ -13,18 +28,17 @@ app.use("/pedidos", rotaPedidos);
 // Quando não encontra a rota, entra aqui:
 app.use((req, res, next) => {
   const erro = new Error("Não encontrado");
-  erro.status= 404;
+  erro.status = 404;
   next(erro);
 });
 
 app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    return res.send({
-        erro:{
-            mensagem: error.message
-        }
-    })
-   
+  res.status(error.status || 500);
+  return res.send({
+    erro: {
+      mensagem: error.message,
+    },
+  });
 });
 
 module.exports = app;
--- routes/produtos.js
+++ routes/produtos.js
@@ -11,8 +11,14 @@ router.get("/", (req, res, next) => {
 
 // INSERE UM PRODUTO
 router.post("/", (req, res, next) => {
+
+    const produto = {
+        nome: req.body.nome,
+        preco: req.body.preco
+    }
   res.status(201).send({
     mensagem: "O produto foi criado",
+    produtoCriado: produto
   });
 });
 
